b4cf5142835aeb5f0abbf230024c43a5f55f8286
yoseli <narutoyhinata22xd@gmail.com>
2026-04-27 08:51:40 -0300

    cambio2 yose
    si

--- github code visual.py
+++ github code visual.py
@@ -1,3 +1,7 @@
 """hola equipoo"""
+array=0
 rango = int(input("ingresar cantidad de veces que desea comprar: "))
 
+for i in range(rango):
+    ventas= [rango]
+    monto= float(input("Ingrese el monto de sus productos"))
