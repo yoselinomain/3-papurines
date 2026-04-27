16ac3ee2395c04763a7a810e50c5ceb2112d0722
denichuuu <yanngfdeleon@gmail.com>
2026-04-27 09:24:52 -0300

    ultimo cambio
    lucas 6:36-38
    
    Co-Authored-By: GadielB <278968589+GadielB@users.noreply.github.com>
    Co-Authored-By: yoselinomain <278968051+yoselinomain@users.noreply.github.com>

--- github code visual.py
+++ github code visual.py
@@ -1,8 +1,9 @@
 """hola equipoo"""
+array = 0
 rango = int(input("ingresar cantidad de veces que desea comprar: "))
 for i in range(rango):
     ventas= [rango]
-    monto= float(input("Ingrese el monto de sus productos"))
+    monto= float(input(f"Ingrese el monto del producto numero {i+1}:$"))
     ventas.append(monto)
     array+= monto
     print("la suma de la compra de sus productos es de: $",array)
